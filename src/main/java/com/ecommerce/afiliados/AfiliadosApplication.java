60a8160ec8e72065e7ea164e82b5a215c4d9bb25
Diogo gomes <diogogomes467@gmail.com>
2024-09-20 00:05:36 -0400

    first commit

---
+++ src/main/java/com/ecommerce/afiliados/AfiliadosApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.afiliados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfiliadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfiliadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/afiliados/AfiliadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.afiliados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfiliadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
